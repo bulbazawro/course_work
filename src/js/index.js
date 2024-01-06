bb0b6648d0d59db15136897cf0f123d8753fb4d7
Bulbazawro <barwars911@gmail.com>
2024-01-06 23:52:02 +0300

    slider complited, start of main

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,34 @@
-import "slick-carousel";
-import "slick-carousel/slick/slick.min.js";
+/* import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs"; */
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+let right = document.querySelectorAll(".slider-block__right");
+
+right.forEach((elem, idx) => {
+  if (idx % 2 == 0) {
+    elem.classList.add("bg_1");
+  } else {
+    elem.classList.add("bg_2");
+  }
+});
