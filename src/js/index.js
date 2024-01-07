758a99d5965255965b3a2bb4309d0be74e69f0a5
Bulbazawro <barwars911@gmail.com>
2024-01-08 02:13:54 +0300

    fixed media in section team

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-/* import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs"; */
-
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
